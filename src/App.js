bd1ee00236fdeeb85306b710d0abfb4dd9c78186
koobohyeon <goosee115@gmail.com>
2018-06-11 14:27:46 +0900

    component module 추출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-
+import React, { Component } from "react"; // 노드 모듈스 안에 라이브러리 모듈을 쓸 떄는 경로를 안쓴다.
+import TodoItem from './components/TodoItem' // src 폴더 안에 ./ 경로를 써줘야 함.
 let count = 0;
 
 class App extends Component {
@@ -77,9 +77,6 @@ class App extends Component {
             <TodoItem 
              key={todo.id} 
              {...todo}
-            //  id={todo.id}
-            //  body={todo.body}
-            //  complete={todo.complete} 
              onComplete={this.handleTodoItemComplete} 
              onDelete={this.handleTodoItemDelete} 
             />
@@ -91,23 +88,6 @@ class App extends Component {
   }
 }
 
-class TodoItem extends Component {
-  render() {
-    const { id, body, complete, onComplete, onDelete} = this.props;
-    return (
-      <li className={complete ? "complete" : ""} key={id}>
-        {body}
-        <button onClick={e => {
-          onComplete(id);
-        }}>완료</button>
-        <button
-          onClick={e => {
-          onDelete(id)
-          }}
-        >삭제</button>
-      </li>
-    );
-  }
-}
+
 
 export default App;
