cdcb5c062d1365fa2ff5ec4db7eca36d14370fee
koobohyeon <goosee115@gmail.com>
2018-06-13 12:36:22 +0900

    로그인페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import TodoPage from './pages/TodoPage'
+import LoginPage from './pages/LoginPage'
+
 export default class App extends React.Component {
-  render() {
-    return (
-      <TodoPage/>
-    )
+  state = {
+    page: 'login'
+  }
+  goToTodoPage = () => {
+    this.setState({
+      page: 'main'
+    })
+  }
+render() {
+  const {page} = this.state
+  return (
+    <div>
+      {page === 'login' ? (
+        <LoginPage onLogin={this.goToTodoPage}/>
+      ) : (
+        <TodoPage/>
+      )}
+    </div>
+  )
   }
 }
